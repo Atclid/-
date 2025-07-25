dbd5ce7891123e34ce927ff6104390f940dc7d52
Atclid <tp10346@126.com>
2025-07-25 12:37:36 +0800

    Update ModBlocks.java

--- bonsaitree_mod/bonsaitree_forge_mod/src/main/java/com/yourmod/bonsaitree/ModBlocks.java
+++ bonsaitree_mod/bonsaitree_forge_mod/src/main/java/com/yourmod/bonsaitree/ModBlocks.java
@@ -1,29 +1,25 @@
-package com.yourmod.bonsaitree;
+package com.yourmod.bonsaitree.init;
 
+import com.yourmod.bonsaitree.BonsaiCropBlock;
+import com.yourmod.bonsaitree.BonsaiTreeBlock;
+import com.yourmod.bonsaitree.BonsaiMod;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.level.material.Material;
 
 public class ModBlocks {
-    public static final DeferredRegister<Block> BLOCKS = 
-        DeferredRegister.create(ForgeRegistries.BLOCKS, Bonsaitree.MOD_ID);
-    
-    // 盆景作物
-    public static final RegistryObject<Block> BONSAI_CROP = BLOCKS.register("bonsai_crop", 
-        () -> new BonsaiCropBlock(BlockBehaviour.Properties.copy(Blocks.WHEAT)
-            .noCollission()
-            .randomTicks()
-            .instabreak()
-            .sound(SoundType.CROP)));
-            
-    // 盆景树
-    public static final RegistryObject<Block> BONSAI_TREE = BLOCKS.register("bonsai_tree", 
-        () -> new BonsaiTreeBlock(BlockBehaviour.Properties.copy(Blocks.OAK_SAPLING)
-            .strength(0.5f)
-            .sound(SoundType.GRASS)
-            .noOcclusion()));
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, BonsaiMod.MOD_ID);
+
+    public static final RegistryObject<Block> BONSAI_TREE = BLOCKS.register("bonsai_tree",
+            () -> new BonsaiTreeBlock(BlockBehaviour.Properties.of(Material.WOOD)
+                    .strength(2.0F).sound(SoundType.WOOD)));
+
+    public static final RegistryObject<Block> BONSAI_CROP = BLOCKS.register("bonsai_crop",
+            () -> new BonsaiCropBlock(BlockBehaviour.Properties.of(Material.PLANT)
+                    .noCollission().randomTicks().instabreak().sound(SoundType.CROP)));
 }
