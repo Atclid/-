5cbfb0654456a9b4aeb542c30a261e13d3fa2d6a
Atclid <tp10346@126.com>
2025-07-25 12:44:45 +0800

    Update Bonsaitree.java

--- bonsaitree_mod/bonsaitree_forge_mod/src/main/java/com/yourmod/bonsaitree/Bonsaitree.java
+++ bonsaitree_mod/bonsaitree_forge_mod/src/main/java/com/yourmod/bonsaitree/Bonsaitree.java
@@ -1,21 +1,20 @@
 package com.yourmod.bonsaitree;
 
-import net.minecraftforge.common.MinecraftForge;
+import com.yourmod.bonsaitree.init.ModBlocks;
+import com.yourmod.bonsaitree.init.ModItems;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(Bonsaitree.MOD_ID)
-public class Bonsaitree {
+@Mod(BonsaiMod.MOD_ID)
+public class BonsaiMod {
     public static final String MOD_ID = "bonsaitree";
-    
-    public Bonsaitree() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+    public BonsaiMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         
-        // 注册方块和物品
-        ModBlocks.BLOCKS.register(bus);
-        ModItems.ITEMS.register(bus);
-        
-        MinecraftForge.EVENT_BUS.register(this);
+        // 注册物品和方块
+        ModItems.ITEMS.register(modEventBus);
+        ModBlocks.BLOCKS.register(modEventBus);
     }
 }
